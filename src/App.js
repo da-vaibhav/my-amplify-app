4e0210e209d3c8f54312409bcb3deb77d0af3468
da-vaibhav <da-vaibhav@users.noreply.github.com>
2020-03-04 15:29:23 +0530

    amplify added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from "aws-amplify-react";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
