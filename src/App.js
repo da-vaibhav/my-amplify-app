89bb49b88561e95ec3b22f36184bff293e5fedda
da-vaibhav <da-vaibhav@users.noreply.github.com>
2020-03-04 16:42:20 +0530

    api and auth added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import { withAuthenticator } from "aws-amplify-react";
+import { Auth } from "aws-amplify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value})
+  };
+
+  signUp = async () => {
+    const { email, password,  username, phone_number } = this.state;
+    try {
+      await Auth.signUp({ username, password,  attributes: { phone_number, email} });
+      this.setState({ step: 1 });
+      console.log('successfully signed up!');
+    } catch (error) {
+      console.log('error signing up!', error);
+    }
+  };
+
+  confirmSignUp = async () => {
+    const { username, authenticationCode } = this.state;
+    try {
+      await Auth.signUp(username, authenticationCode);
+      console.log('user successfully signed up');
+    } catch (error) {
+      console.log('error confirming signing up!', error);
+    }
+  };
+
+  state = { email: '', password: '',  username: '', phone_number: '', authenticationCode: '', step: 0 };
+
+  render = () => {
+    return (
+      <div className="App">
+        { this.state.step === 0 && <div>
+            <input placeholder="username" type="text" onChange={this.onChange} name="username" />
+            <input placeholder="email" type="text" onChange={this.onChange} name="email" />
+            <input placeholder="password" type="password" onChange={this.onChange} name="password" />
+            <input placeholder="phone number" type="text" onChange={this.onChange} name="phone_number" />
+            <button onClick={this.signUp}>sign up</button>
+        </div> }
+
+        { this.state.step === 1 && <div>
+            <input placeholder="username" type="text" onChange={this.onChange} name="username" />
+            <input placeholder="authentication code" type="text" onChange={this.onChange} name="authenticationCode" />
+            <button onClick={this.confirmSignUp}>confirm sign up</button>
+        </div> }
+
+      </div>
   );
 }
+}
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
